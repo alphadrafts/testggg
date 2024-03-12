4ed80a0c7784fd0cb171923ee4cf678f22080b8d
PravasTheDeveloper <info.pravas.cs@gmail.com>
2024-03-12 20:22:46 +0600

    new

---
+++ app/api/chat/route.ts
@@ -0,0 +1,41 @@
+// route.ts Route Handlers
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+export const runtime = 'edge'; // Provide optimal infrastructure for our API route (https://edge-runtime.vercel.app/)
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(config);
+
+
+// POST localhost:3000/api/chat
+export async function POST(request: Request) {
+    const { messages } = await request.json(); // { messages: [] }
+
+    // messages [{ user and he says "hello there" }]
+    console.log(messages);
+
+    // GPT-4 system message
+    // system message tells GPT-4 how to act
+    // it should always be at the front of your array
+
+    // createChatCompletion (get response from GPT-4)
+    const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo-1106',
+        stream: true,
+        messages: [
+            { role: "system", content: `Act like a Professional writer with natural tone writing and informative writing skill.just write what need no question. Dont repeat anything many time and always check grammer errors. Always write accurate and correct information.
+    
+            Instruction: Write a eassay about bangladesh traffic in APA citation format within 5 page and In Text Citation none.Add References if heading give <h1> and if paragraph give <p> add </br> after all <h1> and <p>.`},
+            ...messages
+        ]
+    })
+
+    // create a stream of data from OpenAI (stream data to the frontend)
+    const stream = await OpenAIStream(response);
+
+    // send the stream as a response to our client / frontend
+    return new StreamingTextResponse(stream);
+}
